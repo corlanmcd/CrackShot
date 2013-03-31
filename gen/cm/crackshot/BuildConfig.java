6f6740bd72849b77e5b01d5aa708d7465b0e1686
Corlan <corlanmcd@gmail.com>
2013-03-31 16:25:50 +0200

    Initial Version

---
+++ gen/cm/crackshot/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cm.crackshot;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/cm/crackshot/AboutActivity.java
@@ -0,0 +1,49 @@
+package cm.crackshot;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class AboutActivity extends Activity implements OnClickListener 
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		
+		registerViewListeners();
+	}
+
+	private void registerViewListeners() 
+	{
+		((Button) findViewById(R.id.AboutActivity_backButton)).setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_about, menu);
+		return true;
+	}
+
+	@Override
+	public void onClick(View view) 
+	{
+		if (view.getId() == R.id.AboutActivity_backButton)
+		{
+			startMainMenuActivityFromIntent();
+		}
+	}
+
+	private void startMainMenuActivityFromIntent() {
+		Intent mainMenuIntent = new Intent(this, MainMenuActivity.class);
+		startActivity(mainMenuIntent);
+		finish();
+	}
+
+}
